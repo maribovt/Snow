0b54aa09a94229ca9f25351f6ff9551a0747fa49
maribovt <maribovt@gmail.com>
2019-08-20 16:49:49 +0300

    small-details

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(window).scroll(function() {
 $(document).ready(function(){
   $("#menu").on("click","a", function (event) {
     //отменяем стандартную обработку нажатия по ссылке
-    event.preventDefault();
+    // event.preventDefault();
 
     //забираем идентификатор бока с атрибута href
     var id  = $(this).attr('href'),
@@ -86,7 +86,25 @@ $('.clients-carousel').slick({
   slidesToShow: 5,
   slidesToScroll: 1,
   arrows: false,
-  dots: false
+  dots: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      }
+    }
+  ]
+
+
 });
 
 $('.post-carousel').slick({
